41aef8c4d4feb1731c454ca576e63d33d8ef8efa
korfriend <korfriend@gmail.com>
2020-09-04 17:38:36 +0900

    update dll files

--- VisMtvApi.h
+++ VisMtvApi.h
@@ -36,25 +36,10 @@ DAMAGE.
 
 #include <string>
 #include <map>
+#include <any>
+#include <list>
 
-//#define VERSION "1.0.a" // released at 19.07.30
-//#define VERSION "1.0.b" // released at 19.08.02
-//#define VERSION "1.0.c" // released at 19.08.14
-//#define VERSION "1.0.d" // released at 19.08.29
-//#define VERSION "1.0.e" // released at 19.09.06
-//#define VERSION "1.0.f" // released at 19.09.10
-//#define VERSION "1.0.g" // released at 19.09.18
-//#define VERSION "1.1.a" // released at 19.10.03
-//#define VERSION "1.1.b" // released at 19.10.18
-//#define VERSION "1.1.c" // released at 19.10.25
-//#define VERSION "1.1.d" // released at 19.10.28
-//#define VERSION "1.1.e" // released at 19.11.01
-//#define VERSION "1.1.f" // released at 19.11.04
-//#define VERSION "1.2.a" // released at 19.11.14
-//#define VERSION "1.2.b" // released at 19.11.18
-//#define VERSION "1.2.c" // released at 19.11.26
-//#define VERSION "1.2.d" // released at 19.12.09
-#define VERSION "1.3.a" // released at 20.07.xx
+#define VERSION "2.0" // released at 20.09.05
 
 // Dongjoon's VisMotive interface.
 namespace vzm
@@ -169,7 +154,10 @@ namespace vzm
 	__dojostatic bool DeinitEngineLib();
 
 	// here, obj_id (without const) is [in/out].. in : when a registered object of obj_id exists, out : when there is no registered object of obj_id
-	__dojostatic bool LoadModelFile(const std::string& filename, int& obj_id, const bool unify_redundancy = false);
+	__dojostatic bool LoadModelFile(const std::string& filename, int& obj_id, const bool unify_redundancy = false); 
+	// data_type "CHAR" "BYTE" "SHORT" "USHORT" "INT" "FLOAT"
+	__dojostatic bool GenerateEmptyVolume(int& vol_id, const int ref_vol_id = 0, const std::string& data_type = "", const double min_v = 0, const double max_v = 0, const double fill_v = 0);
+	__dojostatic bool GenerateEmptyPrimitive(int& prim_id);
 	__dojostatic bool GenerateArrowObject(const float* pos_s, const float* pos_e, const float radius, int& obj_id);
 	// optional : rgb_list (if NULL, this is not used)
 	__dojostatic bool GenerateSpheresObject(const float* xyzr_list, const float* rgb_list, const int num_spheres, int& obj_id);
@@ -188,6 +176,8 @@ namespace vzm
 
 	__dojostatic bool ReplaceOrAddSceneObject(const int scene_id, const int obj_id, const ObjStates& obj_states);
 	__dojostatic bool GetSceneObjectState(const int scene_id, const int obj_id, ObjStates& obj_states);
+	// when empty initializer_list, all objs in the scene are considered.
+	__dojostatic bool GetSceneBoundingBox(const std::initializer_list<int>& io_obj_ids, const int scene_id, float* pos_aabb_min_ws, float* pos_aabb_max_ws);
 	__dojostatic bool RemoveSceneObject(const int scene_id, const int obj_id);
 	__dojostatic bool RemoveScene(const int scene_id);
 	__dojostatic bool DeleteObject(const int obj_id); // the obj is deleted in memory
@@ -212,6 +202,8 @@ namespace vzm
 	// only for the contributor's (by DongJoon Kim) test info.
 	__dojostatic void DebugTestSet(const std::string& _script, const void* _pvalue, const size_t size_bytes, const int scene_id, const int cam_id, const int obj_id = -1);
 	__dojostatic void DisplayConsoleMessages(const bool is_display);
+
+	__dojostatic bool ExecuteModule2(const std::string& module_dll_file, const std::string& dll_function, const std::initializer_list<int>& io_obj_ids, const std::map<std::string, std::any>& parameters);
 }
 
 namespace vzmproc
